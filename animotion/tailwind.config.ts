0e531d0adf65d23f5ae551b0b01413eda1625a93
Stewart Permar <stpermar@gmail.com>
2025-03-26 21:24:35 -0700

    migrated from sveltekit to animotion

---
+++ animotion/tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+	darkMode: false,
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
+export default config
--- sveltekit/src/routes/roll/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Slides from '$lib/deck/Slides.svelte'
-</script>
-
-<svelte:head>
-	<title>Roll Call</title>
-</svelte:head>
-
-<Slides />
