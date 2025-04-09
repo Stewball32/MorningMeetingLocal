d4f555bf727366622f3a1c4d26eaf85b9a36924a
Stewball32 <88294715+Stewball32@users.noreply.github.com>
2025-04-08 17:14:25 -0700

    centered lined paper

--- sveltekit/src/routes/teacher/+layout.svelte
+++ sveltekit/src/routes/teacher/+layout.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
+	import { slideStyle } from '$lib';
 
 	let { children } = $props();
 </script>
 
+<div class="place-items-center">
+	<!-- <h1> Current thing</h1> -->
+	<div class="border-1 relative min-h-screen  place-content-center border-red-500">
+		<img
+			src="/paper.svg"
+			class="
+				absolute
+				aspect-[16/9]
+				w-[320px]
+				max-w-full
+				overflow-hidden
+				sm:w-[640px]
+				md:w-[768px]
+				lg:w-[1024px]
+				xl:w-[1280px]
+				2xl:w-[1536px]
+			"
+			alt=""
+		/>
 
-<div class="relative min-h-screen">
-
-	<img
-	src="/paper.svg"
-	class="
-		absolute
-		aspect-[16/9]
-		w-[320px]
-		sm:w-[640px]
-		md:w-[768px]
-		lg:w-[1024px]
-		xl:w-[1280px]
-		2xl:w-[1536px]
-		max-w-full
-		overflow-hidden
-		"
-	alt=""
-/>
-	{@render children()}
+		<div class={slideStyle + '  outline-[.5px]'}>
+			{@render children()}
+		</div>
+	</div>
 </div>
