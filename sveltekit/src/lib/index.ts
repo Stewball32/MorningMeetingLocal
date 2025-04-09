42edf7b9a3a32a188c7a370f72bb6e13d9a44bdb
Stewart Permar <stpermar@gmail.com>
2025-04-08 17:14:25 -0700

    centered lined paper

--- sveltekit/src/lib/index.ts
+++ sveltekit/src/lib/index.ts
@@ -1,1 +1,49 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+
+const slideStyleArray = [
+	"relative",
+	"aspect-[15/9]",
+	"max-w-full",
+	"rounded-r-[1rem]",
+	"overflow-hidden",
+	// Mobile
+	"w-[294px]",
+	"ml-6",
+	"mr-1",
+	"mt-[1.5px]",
+	"rounded-r-[10px]",
+	// Small
+	"sm:w-[590px]",
+	"sm:ml-12",
+	"sm:mr-1",
+	"mt-[1.5px]",
+	"sm:rounded-r-[1.4rem]",
+	// Medium
+	"md:w-[710px]",
+	"md:ml-14",
+	"md:mr-1",
+	"md:mt-[1.5px]",
+	"md:rounded-r-[1.6rem]",
+	// Large
+	"lg:w-[944px]",
+	"lg:ml-19",
+	"lg:mr-0",
+	"lg:mt-[1.7px]",
+	"lg:rounded-r-[2rem]",
+	// Extra Large
+	"xl:w-[1178px]",
+	"xl:ml-24",
+	"xl:mr-2",
+	"xl:mt-1",
+	"xl:rounded-r-[2.4rem]",
+	// 2 Extra Large
+	"2xl:w-[1400px]",
+	"2xl:ml-28",
+	"2xl:mr-2",
+	"2xl:mt-1",
+	"2xl:rounded-r-[3rem]",
+	
+]
+
+export const slideStyle = " " + slideStyleArray.join(" ") + " "
--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -9,8 +9,16 @@
 	<title>Morning Meeting</title>
 </svelte:head>
 
+<div class="absolute right-0 top-0 z-10 text-xl text-blue-600">
+	<p class="block sm:hidden">XS</p>
+	<p class="hidden sm:block md:hidden">SM</p>
+	<p class="hidden md:block lg:hidden">MD</p>
+	<p class="hidden lg:block xl:hidden">LG</p>
+	<p class="hidden xl:block 2xl:hidden">XL</p>
+	<p class="hidden 2xl:block">2XL</p>
+</div>
 
-<div class="relative min-h-screen">
+<div class="relative min-h-screen min-w-screen">
 
 	{@render children()}
 </div>
