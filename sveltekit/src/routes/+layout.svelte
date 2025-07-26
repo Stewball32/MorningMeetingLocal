e735fbc9afbaf4265227411807a6bac7c67cf94f
stewball32 <stpermar@gmail.com>
2025-07-25 17:22:31 -0700

    added: nav bar and rail wrapper with "show" bar functionality.

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,23 +1,121 @@
 <script lang="ts">
+	import type { LayoutData } from './$types';
 	import '../app.css';
+	import type { Snippet } from 'svelte';
+	import { navTiles, navFooter } from './nav';
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	import { onNavigate } from '$app/navigation';
+	import IconHome from '@lucide/svelte/icons/home';
+	import IconMenu from '@lucide/svelte/icons/menu';
 
-	let { children } = $props();
+	let { data, children, pathname }: { data: LayoutData; children: Snippet; pathname: string } =
+		$props();
+
+	let showMenu = $state(data.showMenu ?? true);
+	const noMenuPaths = $state(data.noMenuPaths ?? []);
+	onNavigate((e) => {
+		showMenu = !noMenuPaths.some((path) => e.to?.url.pathname.startsWith(path));
+		pathname = e.to?.url.pathname ?? '';
+	});
+
+	let isExpansed = $state(false);
+
+	function toggleExpanded() {
+		isExpansed = !isExpansed;
+	}
+
+	let gridClass = $derived(
+		showMenu
+			? 'grid grid-rows-[1fr_auto] border-[1px] md:grid-cols-[auto_1fr] md:grid-rows-none'
+			: ''
+	);
+	let bgClass = $state('bg-gradient-to-br from-blue-400 to-blue-600');
 </script>
 
 <svelte:head>
 	<title>Morning Meeting</title>
 </svelte:head>
 
-<div class="absolute left-0 top-0 z-10 text-xl text-blue-600">
-	<p class="block sm:hidden">XS</p>
-	<p class="hidden sm:block md:hidden">SM</p>
-	<p class="hidden md:block lg:hidden">MD</p>
-	<p class="hidden lg:block xl:hidden">LG</p>
-	<p class="hidden xl:block 2xl:hidden">XL</p>
-	<p class="3xl:hidden hidden 2xl:block">2XL</p>
-	<p class="3xl:block hidden">3XL</p>
-</div>
+<div
+	class={`card border-surface-100-900 h-screen w-screen
+          ${gridClass} ${bgClass}`}
+>
+	{#if showMenu}
+		<Navigation.Rail expanded={isExpansed} classes="hidden md:flex h-full overflow-y-auto ">
+			{#snippet header()}
+				<Navigation.Tile
+					classes="h-10  w-full flex items-center justify-center"
+					expandedClasses="h-10  w-full flex items-center justify-center"
+					onclick={toggleExpanded}
+				>
+					<IconMenu />
+				</Navigation.Tile>
+				<Navigation.Tile
+					labelExpanded="Home"
+					labelExpandedBase="text-2xl h-10"
+					href="/"
+					title="Home"
+					id="/"
+					selected={pathname === '/'}
+				>
+					<IconHome />
+				</Navigation.Tile>
+			{/snippet}
 
-<div class="min-w-screen relative min-h-screen">
-	{@render children()}
+			{#snippet tiles()}
+				{#each navTiles.filter((tile) => tile.rail || tile.railExpanded === isExpansed) as tile}
+					<Navigation.Tile
+						labelExpanded={tile.labelExpanded}
+						labelExpandedBase={tile.rail ? 'text-lg' : 'text-xs'}
+						expandedClasses={tile.rail ? 'mt-4 h-10' : 'pl-10 my-0 py-0 h-10'}
+						href={tile.href}
+						title={tile.label}
+						id={tile.href}
+						selected={pathname === tile.href}
+					>
+						<tile.icon />
+					</Navigation.Tile>
+				{/each}
+			{/snippet}
+
+			{#snippet footer()}
+				{#each navFooter.filter((tile) => tile.rail || tile.railExpanded === isExpansed) as tile}
+					<Navigation.Tile
+						labelExpanded={tile.labelExpanded}
+						labelExpandedBase={tile.rail ? 'text-2xl' : 'text-lg'}
+						expandedClasses={tile.rail ? 'mt-4' : 'pl-10 my-0 py-0'}
+						href={tile.href}
+						title={tile.label}
+						id={tile.href}
+						selected={pathname === tile.href}
+					>
+						<tile.icon />
+					</Navigation.Tile>
+				{/each}
+			{/snippet}
+		</Navigation.Rail>
+	{/if}
+
+	<div class="flex h-full w-full flex-col overflow-y-auto">
+		{@render children()}
+	</div>
+
+	{#if showMenu}
+		<Navigation.Bar classes="md:hidden overflow-x-auto">
+			<Navigation.Tile label="Home" href="/" title="Home" id="/" selected={pathname === '/'}>
+				<IconHome />
+			</Navigation.Tile>
+			{#each [...navTiles, ...navFooter].filter((tile) => tile.bar) as tile}
+				<Navigation.Tile
+					label={tile.label}
+					href={tile.href}
+					title={tile.label}
+					id={tile.href}
+					selected={pathname === tile.href}
+				>
+					<tile.icon />
+				</Navigation.Tile>
+			{/each}
+		</Navigation.Bar>
+	{/if}
 </div>
