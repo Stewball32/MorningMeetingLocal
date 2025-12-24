3a2bdaceaf6ecd036d3c027c18344556a353bda8
Stewball32 <88294715+Stewball32@users.noreply.github.com>
2025-12-23 21:47:04 -0800

    added: structure for emotion and image edit pages.

---
+++ sveltekit/src/routes/(edit)/emotions/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from '../emotions/$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div>
+	<h1>Slides</h1>
+	<p>Data: {JSON.stringify(data)}</p>
+</div>
